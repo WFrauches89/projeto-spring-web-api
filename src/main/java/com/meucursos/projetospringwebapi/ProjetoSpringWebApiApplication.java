6433e62728344a447e26a98001c417777e9ec28c
Wanderson Frauches <wfrauches89@gmail.com>
2024-02-20 09:20:23 -0300

    Initial commit

---
+++ src/main/java/com/meucursos/projetospringwebapi/ProjetoSpringWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.meucursos.projetospringwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meucursos/projetospringwebapi/ProjetoSpringWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meucursos.projetospringwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
